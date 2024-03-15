a47b686ce200946e38a2f1d7be1aa2d803934aa8
Vasco Brazao <brazao.v@gmail.com>
2024-03-15 17:52:05 +0000

    also create list of descriptive tables per category of post

--- 02_analysis-codes/r/01_descriptive-table.R
+++ 02_analysis-codes/r/01_descriptive-table.R
@@ -15,26 +15,50 @@ library(gtsummary)
 
 
 # load the data
-dat <- readRDS(file = here::here("01_data-processing", "data", "data_final.rds"))
-
-tab <- dat |> 
+dat <- readRDS(file = here::here("01_data-processing", "data", "data_final.rds")) |> 
   dplyr::rename_with(
     .fn = ~ stringr::str_replace(.x, pattern = "_", replacement = " ") |> 
       stringr::str_to_title() 
-  ) |> 
+  ) 
+
+tab <- dat |> 
   dplyr::reframe(
     dplyr::across(
-      .cols = c(Interactions, "Page Followers", "Pos Sentiment", "Neg Sentiment"),
+      .cols = c(Interactions, "Pos Sentiment", "Neg Sentiment"),
       .fns = list("Mean" = mean, "SD" = sd),
       .names = "{.col}.{.fn}"
     ),
     .by = University
   )
 
+# do the same but creating a table for each category
+tab_cat_list <- levels(dat$Category) |> 
+  purrr::set_names() |> 
+  purrr::map(
+    .f = ~ dat |> 
+      dplyr::filter(
+        Category == .x
+      ) |> 
+      dplyr::reframe(
+        dplyr::across(
+          .cols = c(Interactions, "Page Followers", "Pos Sentiment", "Neg Sentiment"),
+          .fns = list("Mean" = mean, "SD" = sd),
+          .names = "{.col}.{.fn}"
+        ),
+        .by = University
+      ), 
+  )
+
 # save table
 saveRDS(
   object = tab,
   file = here::here("02_analysis-codes", "outputs", "table_descriptives.rds")
+)
+
+# save list of tables
+saveRDS(
+  object = tab_cat_list,
+  file = here::here("02_analysis-codes", "outputs", "table_categories_descriptives.rds")
 )
 
 # clean environment

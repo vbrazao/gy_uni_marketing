85f1354859d0062f179a1913cc37158380256e30
Vasco Brazao <brazao.v@gmail.com>
2024-04-08 11:38:54 +0100

    working on timeline graph

--- 02_analysis-codes/r/03_generate-timeline-graph.R
+++ 02_analysis-codes/r/03_generate-timeline-graph.R
@@ -19,7 +19,7 @@ dat <- readRDS(file = here::here("01_data-processing", "data", "data_final.rds")
 
 
 timeline_start <- lubridate::dmy("07-10-2023") |> lubridate::as_datetime()
-timeline_end <- lubridate::dmy("15-02-2024") |> lubridate::as_datetime()
+timeline_end <- lubridate::dmy("08-02-2024") |> lubridate::as_datetime()
 
 dat |> 
   dplyr::filter(
@@ -46,7 +46,17 @@ dat |>
   mutate(
     date_created = lubridate::as_datetime(date_created)
   ) |> 
-  ggplot(aes(x = date_created, y = university, color = category)) +
-  geom_jitter(height = .1, width = 0, alpha = .7) +
+  ggplot(aes(x = date_created, y = category, color = category)) +
+  geom_jitter(height = 0, width = 0, alpha = .6) +
+  scale_color_viridis_d(option = "A", end = 0.8) +
   coord_cartesian(xlim = c(timeline_start,
-                           timeline_end))
+                           timeline_end)) +
+  geom_vline(xintercept = lubridate::dmy("07-10-2023") |> as_datetime(), size = .5, linetype = "dotted") +
+  facet_grid(university ~ ., labeller = label_wrap_gen(10)
+            ) +
+  labs(x = "", y = "") +
+  theme(
+    legend.position = "none",
+    strip.text.y = element_text(angle = 0), 
+    panel.grid.minor.x = element_blank()
+  )
